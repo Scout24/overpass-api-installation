bc86c16800a647f5d4751dee68d330aa0da62ad1
Torsten Compart <torsten.compart@scout24.com>
2018-01-21 11:45:02 +0100

    Returning only exit code if really something happend.

--- install_all.sh
+++ install_all.sh
@@ -24,19 +24,19 @@ function installNginx {
   if service --status-all 2>&1 | grep -Fq 'nginx'; then
     [ -f ${NGINX_CONF} ] && cp -f ${NGINX_CONF} ${TMP}/nginx.conf
     [ -f ./src/nginx.conf ] && [ -f ${NGINX_CONF} ] && cp -f ./src/nginx.conf ${NGINX_CONF}
-    service nginx restart 2>&1 >/dev/null || echo "NGINX Restart failed!" && exit 2;
+    service nginx restart 2>&1 >/dev/null || (echo "NGINX Restart failed!" && exit 2);
   fi
 }
 
 function installOverpassAPI {
-  ./src/install ${EXEC_DIR} || echo "Overpass Installation failed!" && exit 4
+  ./src/install ${EXEC_DIR} || (echo "Overpass Installation failed!" && exit 4)
   [ ! -d DB_DIR ] && mkdir -p 744 ${DB_DIR}
 }
 
 function installOverpassServer {
   cp -f ./src/overpass /etc/init.d/overpass
   mkdir -p -m 744 /etc/overpass && cp ./src/conf.sh /etc/overpass/
-  service overpass start 2>&1 >/dev/null || echo "Overpass Start failed!" && exit 3;
+  service overpass start 2>&1 >/dev/null || (echo "Overpass Start failed!" && exit 3);
 }
 
 echo "Installing components"
